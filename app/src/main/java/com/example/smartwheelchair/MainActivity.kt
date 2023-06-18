66098a502ed41fd0a80b20913bf0426c44cf8e56
HARSH <theharsh1210@gmail.com>
2023-06-18 14:49:36 +0530

    log and signup complete

--- app/src/main/java/com/example/smartwheelchair/MainActivity.kt
+++ app/src/main/java/com/example/smartwheelchair/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.smartwheelchair
 
+import android.app.AlertDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val logout = findViewById<Button>(R.id.logout_btn)
+        logout.setOnClickListener {
+            AlertDialog.Builder(this@MainActivity)
+                .setTitle("Saarthi")
+                .setMessage("Are you sure you want to exit?")
+                .setCancelable(false)
+                .setPositiveButton("Yes") { dialog, id ->
+                    FirebaseAuth.getInstance().signOut()
+                    val intent = Intent(applicationContext, login::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+                .setNegativeButton("No", null)
+                .show()
+
+        }
     }
 }
--- app/src/main/java/com/example/smartwheelchair/login.kt
+++ app/src/main/java/com/example/smartwheelchair/login.kt
@@ -1,11 +1,90 @@
 package com.example.smartwheelchair
 
+import android.app.ProgressDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class login : AppCompatActivity() {
+
+    private var mAuth: FirebaseAuth? = null
+    private var authStateListener: FirebaseAuth.AuthStateListener? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val signup = findViewById<TextView>(R.id.sign_up_btn)
+
+        signup.setOnClickListener {
+            val i = Intent(applicationContext , SignupActivity::class.java)
+            startActivity(i)
+        }
+
+        mAuth = FirebaseAuth.getInstance()
+        var progressDialog = ProgressDialog(this)
+
+        authStateListener = FirebaseAuth.AuthStateListener {
+            val user: FirebaseUser? = mAuth!!.getCurrentUser()
+            if (user != null) {
+                val intent2 = Intent(applicationContext, MainActivity::class.java)
+                startActivity(intent2)
+                finish()
+            }
+        }
+        val email = findViewById<EditText>(R.id.email_edt_text)
+        val password = findViewById<EditText>(R.id.pass_edt_text)
+
+
+        val login = findViewById<View>(R.id.login_btn)
+        login.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View?) {
+                val emailString = email.text.toString()
+                val passwordString = password.text.toString()
+                if (TextUtils.isEmpty(emailString)) {
+                    email.error = "Email is required"
+                }
+                if (TextUtils.isEmpty(passwordString)) {
+                    password.error = "Password is required"
+                } else {
+                    progressDialog.setMessage("login in progress")
+                    progressDialog.setCanceledOnTouchOutside(true)
+                    progressDialog.show()
+                    mAuth!!.signInWithEmailAndPassword(emailString, passwordString)
+                        .addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                val intent2 = Intent(applicationContext, MainActivity::class.java)
+                                startActivity(intent2)
+                                finish()
+                                progressDialog.dismiss()
+                            } else {
+                                Toast.makeText(
+                                    applicationContext,
+                                    task.exception.toString(),
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                                progressDialog.dismiss()
+                            }
+                        }
+                }
+            }
+        })
+    }
+
+    override fun onStart() {
+        super.onStart()
+        authStateListener?.let { mAuth?.addAuthStateListener(it) }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        authStateListener?.let { mAuth?.removeAuthStateListener(it) }
     }
 }
